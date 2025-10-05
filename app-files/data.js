7c2ea94699788d6421e0054a328497bdc05125cf
Slerrick <zaharegorbolobolov@gmail.com>
2025-10-05 17:51:32 +0300

    Test commit

---
+++ app-files/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-862f1e47ac2a41304556fa95b87ec871",
+      "name": "862f1e47ac2a41304556fa95b87ec871",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "yaw": 0.02552028606653778,
+        "pitch": -0.1269351119538058,
+        "fov": 1.2718787058338181
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.3052392263912367,
+          "pitch": 0.17655661541074785,
+          "title": "Ещкере",
+          "text": "Ратататата да сука это на века"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
